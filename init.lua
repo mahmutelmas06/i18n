215bc082c0208985896024b2c08f9c3c07f410a6
MrIbby <siribby@outlook.com>
2015-11-10 21:31:48 -0800

    Improved stuff and re-ordered en_US

--- init.lua
+++ init.lua
@@ -1,60 +1,81 @@
 -- i18n = internationalization
 -- l10n = localization
 
+if rawget(_G, "i18n") then return end
+
 i18n = {}
-i18n.locale = minetest.setting_get("locale") or "en_US"
+i18n.locale = minetest.setting_get("locale")
 i18n.l10n = {}
 
-minetest.log("info", "Locale: "..i18n.locale)
-
-function i18n.format(s, args)
-	return i18n.l10n[i18n.locale][s] or i18n.l10n["en_US"][s] or s
+function i18n.format(s, default, args)
+	local formatted = default or s
+	
+	if i18n.locale then
+		formatted = i18n.l10n[s] or formatted
+	end
+	
+	if args then
+		formatted = formatted:format(args)
+	end
+	
+	return formatted
 end
 
-local supportedmods = {"beds", "boats", "bones", "bucket", "creative", "default", "doors", "dye", "farming", "fire", "flowers", "give_initial_stuff", "screwdriver", "sethome", "stairs", "tnt", "vessels", "wool", "xpanes"}
-
-local function get_path(...)
-    local separator = package.config:sub(1,1);
-    local elements = {...}
-    return table.concat(elements, separator)
-end
-
-local function file_exists(name)
-	return os.rename(name, name) ~= nil
-end
-
-local function list_files(dir)
-	-- Unix: ls -a dir
-	return io.popen('dir "'..dir..'" /b'):lines()
-end
-
-for i, modname in ipairs(minetest.get_modnames()) do
-	local langdir = get_path(minetest.get_modpath(modname), "lang")
-	if file_exists(langdir) then
-		for filename in list_files(langdir) do
-			local lang, extension = filename:match("([^\\/]-)(%.?[^%.\\/]*)$")
-			if extension == ".lua" then
-				if i18n.l10n[lang] == nil then
-					i18n.l10n[lang] = {}
+function i18n.localize_mod(modname)
+	if i18n.locale then
+		modname = modname or minetest.get_current_modname()
+		for name, def in pairs(minetest.registered_items) do
+			if def.description and def.description ~= "" then
+				if name:match("^"..modname..":") then
+					minetest.override_item(name, {
+						description = i18n.format(s, def.description)
+					})
 				end
-				
-				local localization = dofile(get_path(langdir, filename))
-				for k, v in pairs(localization) do
-					i18n.l10n[lang][k] = v
-				end
-				
-				minetest.log("action", "Added localization from "..langdir)
 			end
 		end
 	end
 end
 
-for name, def in pairs(minetest.registered_items) do
-	if def.description ~= nil and def.description ~= "" then
-		for i, modname in ipairs(supportedmods) do
-			if name:match("^"..modname..":") then
-				minetest.override_item(name, {description = i18n.format(name)})
-				break
+if i18n.locale then
+	local function get_path(...)
+		local separator = package.config:sub(1,1);
+		local elements = {...}
+		return table.concat(elements, separator)
+	end
+
+	local function exists(file)
+		return os.rename(file, file)
+	end
+
+	local function list_files(dir)
+		-- Unix: ls -a dir
+		return io.popen('dir "'..dir..'" /b'):lines()
+	end
+	
+	for i, modname in ipairs(minetest.get_modnames()) do
+		local dir = get_path(minetest.get_modpath(modname), "lang")
+		
+		if exists(dir) then
+			for file in list_files(dir) do
+				local locale, filetype = file:match("([^\\/]-)(%.?[^%.\\/]*)$")
+				
+				if locale == i18n.locale and filetype == ".lua" then
+					local l10n = dofile(get_path(dir, file))
+					
+					for name, description in pairs(l10n) do
+						i18n.l10n[name] = description
+						
+						-- Localize item if mod already active
+						local def = minetest.registered_items[name]
+						if def and def.description and def.description ~= "" then
+							minetest.override_item(name, {
+								description = description or def.description
+							})
+						end
+					end
+					
+					minetest.log("action", "Added localization from "..dir)
+				end
 			end
 		end
 	end
